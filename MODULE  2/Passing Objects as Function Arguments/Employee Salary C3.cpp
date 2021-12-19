61658bccd0e916412fb7717420707ec33fb319bc
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-19 17:50:57 +0530

    Create Employee Salary C3.cpp

---
+++ MODULE  2/Passing Objects as Function Arguments/Employee Salary C3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    public:
+        int Salary;
+        int Shares;
+        void setData(){
+            cout<<"Enter Salary: "<<endl;
+            cin>>Salary;
+            cout<<"Enter Shares: "<<endl;
+            cin>>Shares;
+        }
+        void totalData(Employee x,Employee y){
+            Salary = x.Salary + y.Salary;
+            Shares = x.Shares + y.Shares;
+        }
+        void printData(){
+            cout<<" Salary: "<<Salary<<endl;
+            cout<<" Shares: "<<Shares<<endl;
+        }
+        
+        void printTotalData(){
+            cout<<"Total Salary: "<<Salary<<endl;
+            cout<<"Total Shares: "<<Shares<<endl;
+        }
+};
+
+int main(){
+    Employee vaasu,mohit,total;
+    vaasu.setData();
+    vaasu.printData();
+    
+    mohit.setData();
+    mohit.printData();
+    
+    total.totalData(vaasu,mohit);
+    
+    total.printTotalData();
+}
