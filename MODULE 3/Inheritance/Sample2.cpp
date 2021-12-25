551e1ada281d97d9d14b020db999675c0f6bbc91
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-12-25 17:18:06 +0530

    Create Sample2.cpp

---
+++ MODULE 3/Inheritance/Sample2.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    protected:
+
+        int RollNumber;
+    public:
+        void setRollNumber(int x){
+            RollNumber = x;
+        }
+        void getRollNumber(){
+            cout<<"The roll number is:  "<<RollNumber<<endl;
+        }
+};
+
+class Exam : public Student{
+    protected:
+
+        int physics;
+        int chemistry;
+        int maths;
+    public:
+        void setMarks(int p, int c, int m){
+            physics = p;
+            chemistry = c;
+            maths = m;
+        }
+        
+        void getMarks(){
+            cout <<"The marks obtained in physics are: "<<physics<<endl;
+            cout <<"The marks obtained in chemistry are: "<<chemistry<<endl;
+            cout <<"The marks obtained in maths are: "<<maths<<endl;
+        }
+    
+};
+
+class Result : public Exam
+{
+    float percentage;
+
+public:
+    void display_results()
+    {
+        getRollNumber();
+        getMarks();
+        cout << "Your result is " << (maths + physics + chemistry) / 3 << "%" << endl;
+    }
+};
+
+
+int main()
+{
+    Result harry;
+    harry.setRollNumber(420);
+    harry.setMarks(94, 90,99);
+    harry.display_results();
+    return 0;
+}
