27c020682d4ea2d34a4d5e934fbdeb9995a5d3a8
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-02-12 08:55:20 +0530

    Create BankAccount.cpp

---
+++ Experiments/Experiments 1/BankAccount.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+class BankAccount{
+    char name[100];
+    int AccountNumber;
+    char AccountType[100];
+    float Balance;
+    
+    public:
+    void setData(void){
+        cout<<"Enter Name"<<endl;
+        cin>>name;
+        cout<<"Enter account number"<<endl;
+        cin>>AccountNumber;
+        cout<<"Enter Type of account"<<endl;
+        cin>>AccountType;
+        cout<<"Enter Balance"<<endl;
+        cin>>Balance;
+    }
+    void Deposit(void){
+        int money;
+        cout<<"Enter amount to deposit"<<endl;
+        cin>>money;
+        Balance = Balance + money;
+    }
+    void Withdraw(void){
+        int money;
+        cout<<"Enter amount to withdraw"<<endl;
+        cin>>money;
+        if(money>Balance){
+            cout<<"Insufficient Balance"<<endl;
+        }
+        else{
+            Balance = Balance - money;
+        }
+    }
+    void Display(void){
+        cout<<name<<" has "<<Balance<<" balance in the bank account"<<endl;
+    }
+    
+};
+
+int main(){
+    BankAccount a,b,c;
+    a.setData();
+    a.Display();
+    a.Deposit();
+    a.Display();
+    a.Withdraw();
+    a.Display();
+    
+}
